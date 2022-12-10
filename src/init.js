2fb64e66e966feaa5a0f3732355ae59b9af7b66f
deviant <mb.deviant@gmail.com>
2022-12-10 22:59:13 +0300

    create header and navbar

--- src/init.js
+++ src/init.js
@@ -1,3 +1,52 @@
-export default function pageLoad(){
-    console.log('just making sure again:?');
+function createHeader() {
+    const header = document.createElement('header');
+    header.classList.add('header');
+
+    const title = document.createElement('h3');
+    title.textContent = 'coffee&coffee'
+    header.appendChild(title)
+    header.appendChild(navBar());
+
+
+
+    return header
 }
+
+function navBar() {
+    const nav = document.createElement('nav');
+
+    const about = document.createElement('button');
+    about.classList.add('nav-button');
+    about.textContent = 'About';
+
+    const menu = document.createElement('button');
+    menu.classList.add('nav-button');
+    menu.textContent = 'Menu';
+
+    const contact = document.createElement('button');
+    contact.classList.add('nav-button');
+    contact.textContent = 'Contact';
+    
+
+
+
+    nav.appendChild(about);
+    nav.appendChild(menu);
+    nav.appendChild(contact);
+
+    return nav
+}
+
+
+function pageLoad() {
+    console.log('heh');
+    content.appendChild(createHeader());
+
+}
+
+
+export default pageLoad;
+
+
+//create seperate header content bla bla functions, gather them
+//in one function and call from index
