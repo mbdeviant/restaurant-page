521ff9964a4113852866bdaa43ee39d25c8c1df2
deviant <mb.deviant@gmail.com>
2022-12-11 22:33:25 +0300

    import about.js script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import pageLoad from "./init";
+import test from "./about";
+
+
+
 
 pageLoad();
 
+const about = document.getElementById('about-button');
+
+about.addEventListener('click',()=>{
+    console.log('shit rick');
+    test();
+});
