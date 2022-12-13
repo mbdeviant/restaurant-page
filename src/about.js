63c9b463c9301f0b6335a44f7044523a1e551409
deviant <mb.deviant@gmail.com>
2022-12-14 00:41:57 +0300

    create about page script

--- src/about.js
+++ src/about.js
@@ -1,8 +1,35 @@
-function test() {
-    console.log('damn rick');
-    //YES IT WORKS
+
+
+function createAbout() {
+    const container = document.getElementById('container');
+
+    const aboutContainer = document.createElement('div');
+    aboutContainer.classList.add('about-container');
+    aboutContainer.setAttribute('id', 'about-container');
+
+    const title = document.createElement('h3');
+    title.classList.add('home-title');
+    title.textContent = 'About Us';
+
+    const text = document.createElement('p');
+    text.classList.add('home-text');
+    text.innerHTML = `Set out to serve fellow coffee addicts. <br>
+                    coffee&coffee arouse out of necessity. <br>
+                    We couldn't find a decent coffee to enjoy.<br>
+                    So we said, "Fine, we'll do it ourselves."`;
+
+
+    aboutContainer.appendChild(title);
+    aboutContainer.appendChild(text);
+ 
+
+    container.innerHTML = '';
+    container.appendChild(aboutContainer);
 
 };
 
+export default createAbout;
 
-export default test;
+
+
+
