9f3f7c948d0a0c319f648735637f4fd5f31c4eb3
deviant <mb.deviant@gmail.com>
2022-12-14 00:41:18 +0300

    create script for home page

---
+++ src/home.js
@@ -0,0 +1,33 @@
+
+
+function createHome() {
+    const container = document.getElementById('container');
+
+    const homeContainer = document.createElement('div');
+    homeContainer.classList.add('home-container');
+    homeContainer.setAttribute('id', 'home-container');
+
+    const title = document.createElement('h3');
+    title.classList.add('home-title');
+    title.textContent = 'YOUR BEST COMPANION';
+
+    const text = document.createElement('p');
+    text.classList.add('home-text');
+    text.innerHTML = `is usually a cup of coffee. <br> When you wake up, <br> while working,<br>or with a friend.<br>
+                      So make it count, <br> with coffee&coffee.`;
+
+    const button = document.createElement('button');
+    button.classList.add('home-button');
+    button.textContent = 'Visit Store';
+
+    homeContainer.appendChild(title);
+    homeContainer.appendChild(text);
+    homeContainer.appendChild(button);
+
+    container.innerHTML = '';
+    container.appendChild(homeContainer);
+};
+
+
+
+export default createHome;
