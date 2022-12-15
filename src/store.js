83d91252a995b33e6033193d95c8aa0518b693f2
deviant <mb.deviant@gmail.com>
2022-12-15 05:10:04 +0300

    fix typo

--- src/store.js
+++ src/store.js
@@ -5,23 +5,23 @@ function createStore() {
     const storeContainer = document.createElement('div');
     storeContainer.classList.add('store-container');
 
-    const testitem = createItem('COLD BREW BLEND',
+    const item = createItem('COLD BREW BLEND',
         'Made for cold brewing. For those who likes a bitter taste in their coffee. Use cold and filtered water for better experience.');
-    const testitem2 = createItem('BLUE MOUNTAIN BEANS',
+    const item2 = createItem('BLUE MOUNTAIN BEANS',
         'Handpicked from the mountain slopes. From high altitudes and cold temperatures to your cozy and warm desk. Sweet.');
-    const testitem3 = createItem('VOLCANICA',
-     'Good quality, solid taste. This coffee has one of the biggest seeds. Appearance can be misleading but not this time.');
-    const testitem4 = createItem('KONA COFFEE BEAN',
+    const item3 = createItem('VOLCANICA',
+     'Good quality, solid taste. Volcanica has one of the biggest seeds. Appearance can be misleading but not this time.');
+    const item4 = createItem('KONA COFFEE BEAN',
      'This coffee has it\'s own mood. Grown in rich soils, baked with Sun and washed with rain. Right conditions to be perfect.');
 
 
 
 
 
-    storeContainer.appendChild(testitem);
-    storeContainer.appendChild(testitem2);
-    storeContainer.appendChild(testitem3);
-    storeContainer.appendChild(testitem4);
+    storeContainer.appendChild(item);
+    storeContainer.appendChild(item2);
+    storeContainer.appendChild(item3);
+    storeContainer.appendChild(item4);
 
 
     container.innerHTML = '';
@@ -44,7 +44,7 @@ function createItem(title, text) {
     itemText.classList.add('store-item-text');
     itemText.textContent = text;
 
-    itemContainer.appendChild(img)
+    itemContainer.appendChild(img);
     itemContainer.appendChild(itemTitle);
     itemContainer.appendChild(itemText);
 
