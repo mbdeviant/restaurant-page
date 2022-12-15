233ef91f267f56dbd61b4672275aa7d0cc225750
deviant <mb.deviant@gmail.com>
2022-12-15 05:07:18 +0300

    edit devtool property

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 module.exports = {
-    mode:'development',
-    devtool: 'inline-source-map',
+    mode:'production',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
